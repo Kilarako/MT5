34bfc2b7b119e34970c6b821672dabb5148394ca
Kilarako <elswagger32@gmail.com>
2015-01-16 17:49:00 -0800

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ var app = express()
 , server = http.createServer(app);
 
 // Config
-var PORT = process.env.PORT,
+var PORT = 8081,
 	TRACKS_PATH = './client/multitrack/',
-	addrIP = process.env.IP;
+	addrIP = 127.0.0.1;
 
 //User validation
 var auth = express.basicAuth(function(user, pass) {     
